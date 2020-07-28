1a7e805b7786541ac26bb405cc9614a9c5d66691
Merza saputra <66901495+Dzainvina@users.noreply.github.com>
2020-07-29 00:12:15 +0700

    Update function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -8.670021,115.213195";
+$header[] = "X-Location: -8.673".mt_rand(100,999).",115.21".mt_rand(1000,9999);
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
